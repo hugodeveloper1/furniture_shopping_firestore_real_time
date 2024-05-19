33151f87cae69ead2ad33cf6273d2796488e8775
hugodeveloper1 <hugodv.developer@gmail.com>
2024-05-19 18:09:23 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:furniture_shopping_firestore_real_time/credentials.dart';
 import 'package:furniture_shopping_firestore_real_time/presentation/theme/app_theme.dart';
 import 'package:furniture_shopping_firestore_real_time/ui/pages/home/view/home_view.dart';
 
@@ -9,11 +10,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: '',
-      appId: '',
-      messagingSenderId: '',
-      projectId: '',
+    options: FirebaseOptions(
+      apiKey: AppCredentials.apiKey,
+      appId: AppCredentials.appId,
+      messagingSenderId: AppCredentials.messagingSenderId,
+      projectId: AppCredentials.projectId,
     ),
   );
 
